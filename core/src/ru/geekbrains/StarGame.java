0802d8defeecd659e9c0ec250a75d7e11d19ae47
Consoleman108 <49117521+Consoleman108@users.noreply.github.com>
2020-05-06 17:01:38 +0300

    Homework 1

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -5,26 +5,41 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	TextureRegion region;
+
+	float x, y;
+	float width, height;
+	int scrX, scrY, scrWidth,scrHeight;
+	boolean flipX, flipY;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("VH.jpg");
+		region = new TextureRegion(img, 50, 50, 100, 100);
+		x = 0;
+		y = 0;
+		width = img.getWidth();
+		height = img.getHeight();
+		flipX = false;
+		flipY = false;
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.3f, 0.2f, 0.5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
